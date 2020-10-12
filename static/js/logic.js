e0cf6027a57b20c38b681c4703fe7cb30740ba21
Lorenz Mateo <mateo707@hawaii.edu>
2020-10-12 10:57:02 -1000

    added legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -98,6 +98,25 @@ function createMap(earthquakes) {
         collapsed: false
     }).addTo(myMap);
 
-    
+    var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5];
+        
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + magColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+
 };
 
